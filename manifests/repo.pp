4bbc37daabaea278ad98eee750e6fb795ede05ad
Eric Anderson <eric.sysmin@gmail.com>
2013-11-14 01:56:33 -0800

    update

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -11,8 +11,34 @@
 # Sample Usage:
 #
 class zabbix::repo {
+  case $operatingsystem {
+    centos  : { include zabbix::repo::redhat }
+    redhat  : { include zabbix::repo::redhat }
+    debian  : { include zabbix::repo::debian }
+    ubuntu  : { include zabbix::repo::ubuntu }
+    default : { fail("Unrecognized operating system for webserver") }
+  }
+}
+
+class zabbix::repo::centos {
+  yumrepo { 'zabbix':
+    baseurl  => 'http://repo.zabbix.com/zabbix/2.2/rhel/',
+    gpgcheck => '1',
+    gpgkey   => 'http://repo.zabbix.com/RPM-GPG-KEY-ZABBIX'
+  }
+}
+
+class zabbix::repo::redhat {
+  yumrepo { 'zabbix':
+    baseurl  => 'http://repo.zabbix.com/zabbix/2.2/rhel/',
+    gpgcheck => '1',
+    gpgkey   => 'http://repo.zabbix.com/RPM-GPG-KEY-ZABBIX'
+  }
+}
+
+class zabbix::repo::ubuntu {
   apt::source { 'zabbix':
-    location   => 'http://repo.zabbix.com/zabbix/2.0/ubuntu/',
+    location   => 'http://repo.zabbix.com/zabbix/2.2/ubuntu/',
     release    => 'precise',
     repos      => 'main',
     key        => '79EA5ED4',
@@ -20,3 +46,12 @@ class zabbix::repo {
   }
 }
 
+class zabbix::repo::debian {
+  apt::source { 'zabbix':
+    location   => 'http://repo.zabbix.com/zabbix/2.2/debian/',
+    release    => 'precise',
+    repos      => 'main',
+    key        => '79EA5ED4',
+    key_source => 'http://repo.zabbix.com/zabbix-official-repo.key',
+  }
+}
