36e9d3c5a3ea6c64bb87a16c87be30c51a68edff
le9i0nx <le9i0nx@gmail.com>
2014-02-21 17:57:03 +0600

    fix frontend and puppet subjection

--- manifests/frontend/config.pp
+++ manifests/frontend/config.pp
@@ -2,6 +2,9 @@ class zabbix::frontend::config () {
   file { '/etc/zabbix/web/zabbix.conf.php':
     require => Class['zabbix::frontend::install'],
     replace => true,
+    mode => 0640,
+    owner => 'root',
+    group => 'www-data',
     content => template('zabbix/frontend/zabbix.conf.php.erb'),
   }
 }
