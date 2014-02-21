c83849b815862e46124a682d3a29f883f63b3be3
le9i0nx <le9i0nx@gmail.com>
2014-02-21 11:27:27 +0600

    fix frontend templates and config

--- manifests/frontend/config.pp
+++ manifests/frontend/config.pp
@@ -1,2 +1,7 @@
 class zabbix::frontend::config () {
+  file { '/etc/zabbix/web/zabbix.conf.php':
+    require => Class['zabbix::frontend::install'],
+    replace => true,
+    content => template('zabbix/frontend/zabbix.conf.php.erb'),
+  }
 }
